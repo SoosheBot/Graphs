bd9eb4a7ba5ca97ddc4b234a0f4f534addbdeb46
sooshe <sooshe@sooshebot.com>
2020-07-31 22:45:56 -0500

    MVP adv.py

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -14,11 +14,11 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "projects/adventure/maps/test_line.txt"
+# map_file = "projects/adventure/maps/test_line.txt"
 # map_file = "projects/adventure/maps/test_cross.txt"
 # map_file = "projects/adventure/maps/test_loop.txt"
 # map_file = "projects/adventure/maps/test_loop_fork.txt"
-# map_file = "projects/adventure/maps/main_maze.txt"
+map_file = "projects/adventure/maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -42,20 +42,24 @@ visited[player.current_room.id] = player.current_room.get_exits()
 #traverse the room -- when we start off, the number of rooms we have visited will be less than the total number of rooms
 while len(visited) < len(room_graph) - 1:
     if player.current_room.id not in visited:
+
         #add the room to the visited dict and then look for its exits
         visited[player.current_room.id]  = player.current_room.get_exits()
+        
 
         #remove exits one at a time so we don't backtrack before we are all the way done
-        visited[player.current_room.id].remove(backtrack_path[1])
+        visited[player.current_room.id].remove(backtrack_path[-1])
 
     #When we finally run out of rooms we can backtrack
     while len(visited[player.current_room.id]) == 0:
+
         #go back from the way we came in
         backtrack = backtrack_path.pop()
 
         #like Theseus and his ball of yarn, we wind our way back the way we came
         traversal_path.append(backtrack)
-        #move the player in the opposite direction from whence they came
+
+        #move the player in the opposite direction ....from whence they came
         player.travel(backtrack)
     
     #find the first available exit -- which is the one that was most recently added to the list of room entry/exits -- and then update the path
